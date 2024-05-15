7784ca61b1b323a41215ccd746a86feb8152d6f9
hochausy05 <hochausyhp@gmail.com>
2024-05-16 00:16:23 +0700

    new add

---
+++ src/main/java/com/mycompany/socket2_java/Socket2_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.socket2_java;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Socket2_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
